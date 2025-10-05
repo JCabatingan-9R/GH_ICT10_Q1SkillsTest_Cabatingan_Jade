6be72dd903820a47e41558ab67b0f5b8161075d3
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-10-06 06:58:21 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from pyscript import display, document
+
+
+def orderoutput(e):
+    # remove repeat
+    document.querySelector("#output").innerText = ""
+
+    # variables for orderer information
+    firstname = document.getElementById('fname').value
+    lastname = document.getElementById('lname').value
+    address = document.getElementById('address').value
+    phone = document.getElementById('number').value
+
+    svt = document.getElementById('svtls')
+    enha = document.getElementById('enls')
+    bnd = document.getElementById('bndls')
+    ive = document.getElementById('ivels')
+    skz = document.getElementById('skzls')
+
+    svt1 = document.getElementById('svtls').value
+    enha1 = document.getElementById('enls').value
+    bnd1 = document.getElementById('bndls').value
+    ive1 = document.getElementById('ivels').value
+    skz1 = document.getElementById('skzls').value
+
+    # total for order/s
+    total = float(svt1) * svt.checked + float(enha1) * enha.checked + float(bnd1) * bnd.checked + float(ive1) * ive.checked + float(skz1) * skz.checked
+
+    # multisting for orderer information output
+    multi = f"""
+    Order for: {firstname}
+    Address: {address}
+    Contact Number: {phone}
+    Total: Php {total:.2f}
+    """
+
+    # displaying information to output
+    display(multi, target='output')
